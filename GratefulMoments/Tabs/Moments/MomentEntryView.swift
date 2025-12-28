6c36d044ce71f776319ccb16c815452c127e04c5
Askhat <baiaskhat@gmail.com>
2025-12-28 19:14:04 +0500

    add textfields to momentEntryView

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -8,15 +8,33 @@
 import SwiftUI
 
 struct MomentEntryView: View {
+    @State private var title = ""
+    @State private var note = ""
+    
     var body: some View {
-        contentStack    
+        NavigationStack {
+            ScrollView {
+                contentStack
+            }
+            .scrollDismissesKeyboard(.interactively)
+            .navigationTitle("Grateful for")
+        }
     }
     
     var contentStack: some View {
         VStack(alignment: .leading) {
-            Text("Title")
+            //textfield
+            TextField(text: $title) {
+                Text("Title (Required)")
+            }
+            .font(.title.bold())
+            .padding(.top, 48)
+            Divider()
+            //textfield with as many lines of text as user can type in
+            TextField("Log your small wins", text: $note, axis: .vertical)
+                .multilineTextAlignment(.leading)
+                .lineLimit(5...Int.max)
             
-            Text("Note")
         }
         .padding()
     }
