76cbb9ff9b2d3d1f484fa7e865535a11dabdc8dc
Askhat <baiaskhat@gmail.com>
2025-12-28 21:53:36 +0500

    add photoPicker and proceed with selected photo

--- GratefulMoments/Tabs/Moments/MomentEntryView.swift
+++ GratefulMoments/Tabs/Moments/MomentEntryView.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct MomentEntryView: View {
     @State private var title = ""
     @State private var note = ""
+    @State private var imageData: Data?
+    @State private var newImage: PhotosPickerItem?
     
     var body: some View {
         NavigationStack {
@@ -18,6 +21,31 @@ struct MomentEntryView: View {
             }
             .scrollDismissesKeyboard(.interactively)
             .navigationTitle("Grateful for")
+        }
+    }
+    
+    private var photoPicker: some View {
+        PhotosPicker(selection: $newImage) {
+            Group {
+                if let imageData, let uiImage = UIImage(data: imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    Image(systemName: "photo.badge.plus.fill")
+                        .font(.largeTitle)
+                        .frame(height: 250)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(white: 0.4, opacity: 0.32))
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                }
+            }
+        }
+        .onChange(of: newImage) {
+            guard let newImage else { return }
+            Task {
+                imageData = try await newImage.loadTransferable(type: Data.self)
+            }
         }
     }
     
@@ -35,6 +63,7 @@ struct MomentEntryView: View {
                 .multilineTextAlignment(.leading)
                 .lineLimit(5...Int.max)
             
+            photoPicker
         }
         .padding()
     }
