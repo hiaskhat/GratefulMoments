c7db24e970fe477686ea8f08bb2a1fe84c3d165e
Askhat <baiaskhat@gmail.com>
2025-12-28 17:40:32 +0500

    Initial commit

---
+++ GratefulMoments/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GratefulMoments
+//
+//  Created by Асхат Баймуканов on 18.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Askhat!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Асхат Баймуканов on 18.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
