dc3f53b5167cf9a38b2783e42a144d2e9214ef84
Askhat <baiaskhat@gmail.com>
2026-01-01 20:36:49 +0500

    add MomentHexagonView
    добавил вью в котором накинул шестиугольник на наш момент и в превью посмотрел как булет смотреться с картинкой и без: без картинки будет тайтл и заметка, с картинкой - только тайтл. Также внизу добавил дату отформатированнную. Также добавлены размеры отступов и высоты

--- GratefulMoments/Custom Views/HexagonLayout.swift
+++ GratefulMoments/Custom Views/HexagonLayout.swift
@@ -5,7 +5,7 @@
 //  Created by Асхат Баймуканов on 01.01.2026.
 //
 
-import Foundation
+import SwiftUI
 
 enum HexagonLayout {
     case standart
@@ -18,5 +18,36 @@ enum HexagonLayout {
         case .large:
             return 350.0
         }
+    }
+    
+    var timestampBottomPadding: CGFloat {
+        0.08
+    }
+    
+    var textBottomPadding: CGFloat {
+        0.25
+    }
+    
+    var timestampHeight: CGFloat {
+        size * (textBottomPadding - timestampBottomPadding)
+    }
+    
+    var titleFont: Font {
+        switch self {
+        case .standart:
+            return .headline
+        case .large:
+            return .title.bold()
+        }
+    }
+    
+    var bodyFont: Font {
+        switch self{
+        case .standart:
+            return .caption2
+        case .large:
+            return .body
+        }
+        
     }
 }
